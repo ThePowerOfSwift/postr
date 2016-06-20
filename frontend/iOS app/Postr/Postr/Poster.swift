22aa7d723dfcd6b0e57ca03ab362c81f92148b2c
sk3513 <sk3513@ic.ac.uk>
2016-06-20 11:22:40 +0100

    Removed deleted files

---
+++ frontend/iOS app/Postr/Postr/Poster.swift
@@ -0,0 +1,9 @@
+//
+//  Poster.swift
+//  Postr
+//
+//  Created by Steven Kingaby on 20/06/2016.
+//  Copyright © 2016 Steven Kingaby. All rights reserved.
+//
+
+import Foundation
---
+++ frontend/iOS app/Postr/Postr/ZoomView.swift
@@ -0,0 +1,21 @@
+//
+//  ZoomView.swift
+//  Postr
+//
+//  Created by Steven Kingaby on 18/06/2016.
+//  Copyright © 2016 Steven Kingaby. All rights reserved.
+//
+
+import UIKit
+
+class ZoomView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
